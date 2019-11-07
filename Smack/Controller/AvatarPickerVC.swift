92ca937efaf8ad278fef5da519f69a9331352363
OkonovUrmat <okonov.okonov@gmail.com>
2019-11-07 14:10:59 +0600

    Fixed constraints and added AvatarCell

--- Smack/Controller/AvatarPickerVC.swift
+++ Smack/Controller/AvatarPickerVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AvatarPickerVC: UIViewController {
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     //Outlets
     @IBOutlet weak var collectionVIew: UICollectionView!
@@ -16,8 +16,28 @@ class AvatarPickerVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionVIew.delegate = self
+        collectionVIew.dataSource = self
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+        return cell
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Urmat on 11/3/19.
+//  Copyright © 2019 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+ 
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
