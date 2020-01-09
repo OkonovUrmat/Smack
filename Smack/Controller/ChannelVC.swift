658053382a43f7c96e51cab1456eaaa7a4a55515
OkonovUrmat <okonov.okonov@gmail.com>
2020-01-09 19:25:32 +0600

    Changing image in the Slide Menu after creating account

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -13,15 +13,28 @@ class ChannelVC: UIViewController {
     //Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind (segue: UIStoryboardSegue) {}
+    @IBOutlet weak var userImg: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Ширина выпадающиего меню
+        //Slide menu wirdth
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_notif:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataDidChange (_notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle("\(UserDataService.instance.name)", for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
     }
     
 }
