0ed78f78208d69e68fdf4023f5e4b6a0f6502ed6
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-28 20:28:59 +0600

    Refactor RoundedButton

--- Smack/View/RoundedButton.swift
+++ Smack/View/RoundedButton.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @IBDesignable
 class RoundedButton: UIButton {
-
+    
     @IBInspectable var cornerRadius: CGFloat = 3.0 {
         didSet{
             self.layer.cornerRadius = cornerRadius
